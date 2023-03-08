50e75f975d9b937d84bc739b7c40be9ce00705b0
Giuseppe Lo Re <giuseppe.lore@dev4side.com>
2023-03-08 15:55:18 +0100

    Created callback to pass data from child to parent

--- src/api.js
+++ src/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID 3atkOJedn1vg6JkHsCVR65p-1zXp2Lcq70TfxspOPM8'
         },
         params: {
-            query: 'forests'
+            query: term,
         }
     });
 
-    console.log(response);
-    return response;
+    return response.data.results;
 };
 
 export default searchImages;
